d48e0a2461d9c8e934c2d39ec15bf3d86ca6157b
BohdanRomaniuk <bohdan2307@gmail.com>
2019-05-24 00:23:45 +0300

    Working test but wrong training

--- analyzer/test.py
+++ analyzer/test.py
@@ -1,3 +1,20 @@
 import sys
+import pandas as pd
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
 
-print(sys.argv[1])
+model = load_model('./ai/my_model.h5')
+df_test = pd.read_csv(sys.argv[1], delimiter=",")
+df_test = df_test.drop(['id'], axis=1)
+list_sentences_test = df_test["review"]
+
+max_features = 6000
+tokenizer = Tokenizer(num_words=max_features)
+list_tokenized_test = tokenizer.texts_to_sequences(list_sentences_test)
+
+maxlen = 130
+X_te = pad_sequences(list_tokenized_test, maxlen=maxlen)
+prediction = model.predict(X_te)
+
+print(prediction)
